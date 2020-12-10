fcb613f67b8c284a1d37be256c848ae7ecde623c
Gabriel <61502999+Kzzy1@users.noreply.github.com>
2020-12-10 09:08:10 +0100

    main and regsiter
    changes to main and register to make them into separate files

---
+++ lib/register.dart
@@ -0,0 +1,119 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+
+class ListPageState extends StatefulWidget{
+  @override
+  ListPage createState() => ListPage();
+}
+
+String dropdownvalue = "Teacher";
+
+class ListPage extends State<ListPageState>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.green[50],
+        appBar: AppBar(
+          title: Text(
+            "Register",
+            style: TextStyle(
+              fontSize: 30,
+              fontFamily: "Crimson",
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle:true,
+          backgroundColor: Colors.green[600],
+        ),
+        body:  Center(
+          child: Column( mainAxisAlignment: MainAxisAlignment.end, crossAxisAlignment: CrossAxisAlignment.start,
+              children: [Expanded(child: Text("Name:",
+                style: TextStyle(fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              ),
+                Expanded(child: TextField(
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Name',
+                  ),
+                ),),
+                Expanded(child: Text("Password:",
+                  style: TextStyle(fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                ),
+                Expanded(child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),),
+                Expanded(child: Text("Confirm Password:",
+                  style: TextStyle(fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                ),
+                Expanded(child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Confirm Password',
+                  ),
+                ),),
+                Expanded(child: Text("mail:",
+                  style: TextStyle(fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                ),
+                Expanded(child: TextField(
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'mail',
+                  ),
+                ),),
+                Expanded(child: Text("Alias:",
+                  style: TextStyle(fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                ),
+                Expanded(child: TextField(
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Alias',
+                  ),
+                ),),
+                Expanded(child: Text("Role:",
+                  style: TextStyle(fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                ),
+                Expanded(child: DropdownButton<String>(value: dropdownvalue, style: TextStyle(color: Colors.indigo), underline: Container(height:2, color: Colors.purple),
+                  onChanged: (String newValue) {
+                    setState(() {
+                      dropdownvalue = newValue;
+                    });
+                  },
+                  items: <String>['Teacher', 'Student'].map<DropdownMenuItem<String>>((String value) { return DropdownMenuItem<String>(value: value, child: Text(value),);
+                  }).toList(),))]),
+        ));
+  }
+}
+
+
+
+
