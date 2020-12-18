8c74450abe5db16a680a30e993b9068c29f32a8a
ViktorLund9 <vigge65@gmail.com>
2020-12-18 16:12:05 +0100

    Can dynamicly add text fields to multiple choice question

--- lib/createquiz.dart
+++ lib/createquiz.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -16,7 +18,9 @@ class createquizPageState extends StatefulWidget{
 
 class createquizPage extends State<createquizPageState>{
   bool test = false;
+  final formKey = GlobalKey<FormState>();
   String Dropdownquestionvalue = "Multiple choice";
+  static List<String> multiplechoiceanswers = [null];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -94,37 +98,164 @@ class createquizPage extends State<createquizPageState>{
     );
   }
 
-  showInformationDialog (BuildContext context) async {
+  showInformationDialog (BuildContext context) {
     return showDialog(context: context,
         builder: (context){
           return AlertDialog(
+            title: Text('Create question'),
             content: StatefulBuilder(
             builder: (BuildContext context, StateSetter setState){
-            return Column(
+            return Container(height: MediaQuery.of(context).size.height*0.9, width: MediaQuery.of(context).size.width ,
+              child: Column(
                 children: [
                   DropdownButton<String>(value: Dropdownquestionvalue, style: TextStyle(color: Colors.black), underline: Container(height:2, color: Colors.purple),
                     onChanged: (String newValue) {
                       setState(() {
                         Dropdownquestionvalue = newValue;
+
                       });
                     },
                     items: <String>['Multiple choice','Written answer','Pair options'].map<DropdownMenuItem<String>>((String value) { return DropdownMenuItem<String>(value: value, child: Text(value),);
                     }).toList(),),
-                ]
-            );}),
-            actions: <Widget> [
-              TextButton(
-                child: Text ("Okay"),
-                onPressed: (){
-                  Navigator.of(context).pop();
-                },
+                Expanded(child: selectform())
+                ],
               ),
-            ],
-          );
-        });
+            );}
+          )
+          );});
   }
+
+  Widget selectform(){
+
+    if(Dropdownquestionvalue == 'Multiple choice'){
+      return Form(
+        key: formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(decoration: InputDecoration(hintText: 'Write question here'),),
+             Text('Add Friends', style: TextStyle(
+          fontWeight: FontWeight.w700, fontSize: 16),),
+          ..._getFriends(),
+            SizedBox(height: 40,),
+
+          ],
+      ));
+    }
+    else if(Dropdownquestionvalue == 'Written answer'){
+      return Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(decoration: InputDecoration(hintText: 'Write question here'),)
+            ],
+          )
+      );
+    }
+    else{
+      return Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(decoration: InputDecoration(hintText: 'Write question here'),)
+            ],
+          )
+      );
+    }
 
 }
 
+  List<Widget> _getFriends(){
+    List<Widget> MCTextFieldsList = [];
+    for(int i=0; i<multiplechoiceanswers.length; i++){
+      MCTextFieldsList.add(
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Row(
+              children: [
+                Expanded(child: mchoicefields(i)),
+                SizedBox(width: 16,),
+                // we need add button at last friends row only
+                _addRemoveButton(i == multiplechoiceanswers.length-1, i),
+              ],
+            ),
+          )
+      );
+    }
+    return MCTextFieldsList;
+  }
+
+  Widget _addRemoveButton(bool add, int index){
+    return InkWell(
+      onTap: (){
+        if(add){
+          multiplechoiceanswers.insert(0, null);
+        }
+        else {multiplechoiceanswers.removeAt(index);}
+        setState((){});
+
+      },
+      child: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          color: (add) ? Colors.green : Colors.red,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Icon(
+          (add) ? Icons.add : Icons.remove, color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+
+}
+class mchoicefields extends StatefulWidget{
+
+  final int index;
+  mchoicefields(this.index);
+
+  @override
+  _mchoicefieldsState createState() => _mchoicefieldsState();
+}
+
+
+
+class _mchoicefieldsState extends State<mchoicefields>{
+
+  TextEditingController _namecontroller;
+
+  @override
+  void initState(){
+    super.initState();
+    _namecontroller = TextEditingController();
+  }
+
+  @override
+  void dispose(){
+    _namecontroller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      _namecontroller.text = createquizPage.multiplechoiceanswers[widget.index]??'';
+    });
+
+    return TextFormField(controller: _namecontroller,
+    onChanged: (v) => createquizPage.multiplechoiceanswers[widget.index] = v,
+      decoration: InputDecoration(hintText: 'Enter Answer'),
+      validator: (v){
+      if(v.trim().isEmpty) return 'Please enter something';
+      return null;
+      },
+    );
+  }
+}
 
 
