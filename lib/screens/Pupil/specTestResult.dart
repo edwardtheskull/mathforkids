afaa55921be24d8ef4d5ca2d001463f06cc62890
oskarrenefalk <61052700+oskarrenefalk@users.noreply.github.com>
2020-12-21 15:32:32 +0100

    Update specTestResult.dart

--- lib/screens/Pupil/specTestResult.dart
+++ lib/screens/Pupil/specTestResult.dart
@@ -16,6 +16,197 @@ class specTestPage extends State<specTestResultState> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
+      backgroundColor: Color.fromRGBO(31, 69, 82, 1),
+      appBar: AppBar(
+        title: Text(
+          "Math for Kids",
+          style: TextStyle(
+            fontSize: SizeConfig.AppbarFontSize,
+            fontFamily: "Architect",
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.green[600],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+
+          children:
+          [Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("Addition diagnos 2", style: TextStyle(
+                fontSize: SizeConfig.HeaderTextFontSize,
+                fontFamily: "Architect",
+                backgroundColor: Colors.green[600],
+                color: Colors.white),),
+          ),
+            Container(
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                color: Color.fromRGBO(31, 69, 82, 1),
+                elevation: 10,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ListTile(
+                      leading: Icon(
+                        Icons.verified,
+                        color: Colors.green,
+                        size: SizeConfig.SmallIconSize,
+                      ),
+                      title: Text('Test Passed',
+                          style: TextStyle(color: Colors.white, fontSize: SizeConfig.MiniTextFontSize)),
+                      subtitle: Text(
+                          '2020-12-12', style: TextStyle(color: Colors.white, fontSize: SizeConfig.XSMiniTextFontSize)),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          InkWell(
+                            child: Text(
+                              "15/16",
+                              style: TextStyle(
+                                  color: Colors.green, fontSize: SizeConfig.MiniTextFontSize),
+                            ),
+                            onTap: (){Navigator.push(context,
+                                new MaterialPageRoute(
+                                    builder: (context) => new specTestResultState(),
+                                  settings: RouteSettings(
+                                    arguments: ListTile
+                                  )),
+                                );
+                            },
+                          )],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0) ,
+              child: Text("Question by question", style: TextStyle(
+                  fontSize: SizeConfig.HeaderTextFontSize,
+                  fontFamily: "Architect",
+                  backgroundColor: Colors.green[600],
+                  color: Colors.white),
+              ),
+            ),
+            Container(
+              child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  color: Color.fromRGBO(31, 69, 82, 1),
+                  elevation: 10,
+                  child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ListTile(
+                          leading: Icon(
+                            Icons.check_circle,
+                            color: Colors.green,
+                            size: SizeConfig.SmallIconSize,
+                          ),
+                          title: Text('Question 1', style: TextStyle(
+                              color: Colors.white, fontSize: SizeConfig.MiniTextFontSize)),
+                          subtitle: Text('1+1=2', style: TextStyle(
+                              color: Colors.white, fontSize: SizeConfig.XSMiniTextFontSize)),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              InkWell(
+                                  child: Text(
+                                    "passed",
+                                    style: TextStyle(
+                                        color: Colors.green, fontSize: SizeConfig.MiniTextFontSize),
+                                  ),
+                                  onTap: () {}),
+                            ],
+                          ),
+                        ),
+                        ListTile(
+                          leading: Icon(
+                            Icons.error,
+                            color: Colors.green,
+                            size: SizeConfig.SmallIconSize,
+                          ),
+                          title: Text('Question 2', style: TextStyle(
+                              color: Colors.white, fontSize: SizeConfig.MiniTextFontSize )),
+                          subtitle: Text('2+2=5', style: TextStyle(
+                              color: Colors.white, fontSize: SizeConfig.XSMiniTextFontSize)),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                InkWell(
+                                    child: Text(
+                                      "view error",
+                                      style: TextStyle(
+                                          color: Colors.green, fontSize: SizeConfig.MiniTextFontSize),
+                                    ),
+                                    onTap: () {}),
+                              ],
+                            )
+                        ),
+                        ListTile(
+                            leading: Icon(
+                              Icons.check_circle,
+                              color: Colors.green,
+                              size: SizeConfig.SmallIconSize,
+                            ),
+                            title: Text('Question 3', style: TextStyle(
+                                color: Colors.white, fontSize: SizeConfig.MiniTextFontSize)),
+                            subtitle: Text('5+5=10', style: TextStyle(
+                                color: Colors.white, fontSize: SizeConfig.XSMiniTextFontSize)),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                InkWell(
+                                    child: Text(
+                                      "passed",
+                                      style: TextStyle(
+                                          color: Colors.green, fontSize: SizeConfig.MiniTextFontSize),
+                                    ),
+                                    onTap: () {}),
+                              ],
+                            )
+                        ),
+                        ListTile(
+                            leading: Icon(
+                              Icons.check_circle,
+                              color: Colors.green,
+                              size: SizeConfig.SmallIconSize,
+                            ),
+
+                            title: Text('Question 4', style: TextStyle(
+                                color: Colors.white, fontSize: SizeConfig.MiniTextFontSize)),
+                            subtitle: Text('15+16=31', style: TextStyle(
+                                color: Colors.white, fontSize: SizeConfig.XSMiniTextFontSize)),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                InkWell(
+                                    child: Text(
+                                      "passed",
+                                      style: TextStyle(
+                                          color: Colors.green, fontSize: SizeConfig.MiniTextFontSize),
+                                    ),
+                                    onTap: () {}),
+                              ],
+                            )
+                        ),
+                      ]
+                  )
+              ),
+            ),
+          ],
+        ),
+      ),
 
     );
   }
