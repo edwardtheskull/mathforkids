1126991571a692de61fe3a368760e826815308b6
Gabriel <61502999+Kzzy1@users.noreply.github.com>
2020-12-22 13:33:36 +0100

    Update login.dart

--- lib/screens/Authenticate/login.dart
+++ lib/screens/Authenticate/login.dart
@@ -47,84 +47,80 @@ class LoginPage extends State<loginPageState>{
           centerTitle:true,
           backgroundColor: Colors.green[600],
         ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Center(
-                  child: Container(width: MediaQuery.of(context).size.width*0.8,
-                  child: Column(mainAxisAlignment: MainAxisAlignment.center,
-                      children: [Row(children: [
-                          Expanded(child: Text("email/username:",
-                            style: TextStyle(fontSize: SizeConfig.TextFontSize, color: Colors.white,
-                                fontWeight: FontWeight.bold, fontFamily: 'Architect'
-                            ),
-                          ),
-                          ),
-                        ],
-                      ),
-                        Row(
-                          children: [
-                            Expanded(child: TextField(controller: textname, style: TextStyle(color: Colors.white, fontFamily: 'Architect', fontSize: SizeConfig.TextFieldFontSize), cursorColor: Colors.white, obscureText: false,
-                              decoration: InputDecoration(enabledBorder: const OutlineInputBorder(borderSide: const BorderSide(color: Colors.white)),
-                                border: OutlineInputBorder(), labelStyle: TextStyle(color: Colors.white),
-                              focusedBorder: OutlineInputBorder(borderSide: const BorderSide(color: Colors.white))
-                              ),
-                            ),),
-                          ],
+        body: SingleChildScrollView(
+          child: Center(
+              child: Container(width: MediaQuery.of(context).size.width*0.8, height: SizeConfig.ScreenHeight*0.6,
+              child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Row(children: [
+                      Expanded(child: Text("email/username:",
+                        style: TextStyle(fontSize: SizeConfig.TextFontSize, color: Colors.white,
+                            fontWeight: FontWeight.bold, fontFamily: 'Architect'
                         ),
-                        Row(
-                          children: [
-                            Expanded(child: Text("Password:",
-                              style: TextStyle(fontSize: SizeConfig.TextFontSize, color: Colors.white,
-                                  fontWeight: FontWeight.bold, fontFamily: 'Architect'
-                              ),
-                            ),
+                      ),
+                      ),
+                    ],
+                  ),
+                    Row(
+                      children: [
+                        Expanded(child: TextField(controller: textname, style: TextStyle(color: Colors.white, fontFamily: 'Architect', fontSize: SizeConfig.TextFieldFontSize), cursorColor: Colors.white, obscureText: false,
+                          decoration: InputDecoration(enabledBorder: const OutlineInputBorder(borderSide: const BorderSide(color: Colors.white)),
+                            border: OutlineInputBorder(), labelStyle: TextStyle(color: Colors.white),
+                          focusedBorder: OutlineInputBorder(borderSide: const BorderSide(color: Colors.white))
+                          ),
+                        ),),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Expanded(child: Text("Password:",
+                          style: TextStyle(fontSize: SizeConfig.TextFontSize, color: Colors.white,
+                              fontWeight: FontWeight.bold, fontFamily: 'Architect'
+                          ),
+                        ),
 
-                          ),
-                        ],
                       ),
-                        Row(
-                          children: [
-                            Expanded(child: TextField(controller: textpassword, style: TextStyle(color: Colors.white, fontFamily: 'Architect', fontSize: SizeConfig.TextFieldFontSize), cursorColor: Colors.white,
-                              obscureText: true,
-                              decoration: InputDecoration(enabledBorder: const OutlineInputBorder(borderSide: const BorderSide(color: Colors.white)),
-                                border: OutlineInputBorder(), labelStyle: TextStyle(color: Colors.white),
-                                focusedBorder: OutlineInputBorder(borderSide: const BorderSide(color: Colors.white))
-                              ),
-                            ),),
-                          ],
+                    ],
+                  ),
+                    Row(
+                      children: [
+                        Expanded(child: TextField(controller: textpassword, style: TextStyle(color: Colors.white, fontFamily: 'Architect', fontSize: SizeConfig.TextFieldFontSize), cursorColor: Colors.white,
+                          obscureText: true,
+                          decoration: InputDecoration(enabledBorder: const OutlineInputBorder(borderSide: const BorderSide(color: Colors.white)),
+                            border: OutlineInputBorder(), labelStyle: TextStyle(color: Colors.white),
+                            focusedBorder: OutlineInputBorder(borderSide: const BorderSide(color: Colors.white))
+                          ),
+                        ),),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Row(mainAxisAlignment: MainAxisAlignment.center,
+                          children: [Padding(
+                          padding: const EdgeInsets.only(top: 35),
+                          child: ButtonTheme(minWidth: SizeConfig.SmallButtonWidth, height: SizeConfig.ButtonHeight,
+                            child: RaisedButton(elevation: 3,
+                              onPressed: () async{
+                                dynamic res = await _auth.signInWithEmailAndPassword(textname.text, textpassword.text);
+                                if(res == null){
+                                  print('error signing in');
+                                } else {
+                                  print('signed in');
+                                  print(res.uid);
+                                  Navigator.push(context, new MaterialPageRoute(builder: (context) => new studentPageState()));
+                                }
+                              },
+                              color: Colors.green[600],
+                              child: Text("Next", style: TextStyle(letterSpacing: 1,
+                                fontSize: SizeConfig.ButtonTextSize,
+                                color: Colors.white,fontFamily: 'Architect',)),),
+                          ),
+                        )],
                         ),
-                        Column(
-                          children: [
-                            Row(mainAxisAlignment: MainAxisAlignment.center,
-                              children: [Padding(
-                              padding: const EdgeInsets.only(top: 35),
-                              child: ButtonTheme(minWidth: SizeConfig.SmallButtonWidth, height: SizeConfig.ButtonHeight,
-                                child: RaisedButton(elevation: 3,
-                                  onPressed: () async{
-                                    dynamic res = await _auth.signInWithEmailAndPassword(textname.text, textpassword.text);
-                                    if(res == null){
-                                      print('error signing in');
-                                    } else {
-                                      print('signed in');
-                                      print(res.uid);
-                                      Navigator.push(context, new MaterialPageRoute(builder: (context) => new studentPageState()));
-                                    }
-                                  },
-                                  color: Colors.green[600],
-                                  child: Text("Next", style: TextStyle(letterSpacing: 1,
-                                    fontSize: SizeConfig.ButtonTextSize,
-                                    color: Colors.white,fontFamily: 'Architect',)),),
-                              ),
-                            )],
-                            ),
-                          ],
-                        ),
-                      ]),
-                ),
-              ),
+                      ],
+                    ),
+                  ]),
             ),
-          ],
+          ),
         )
     );
   }
