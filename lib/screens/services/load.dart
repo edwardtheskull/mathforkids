13990b2b3c63a3bd3069e96e65108e04f1b4fa16
Gabriel <61502999+Kzzy1@users.noreply.github.com>
2021-01-01 14:36:44 +0100

    Added a different colour to the loading screen

--- lib/screens/services/load.dart
+++ lib/screens/services/load.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:mathforkids/ChangeTheme.dart';
 
 class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color.fromRGBO(31, 69, 82, 1),
+      color: setTheme.scaffoldBackgroundColor,
       child: Center(
         child: SpinKitFadingCircle(
-          color: Colors.green[600],
+          color: setTheme.primaryColor,
           size: 50.0,
         ),
       ),
