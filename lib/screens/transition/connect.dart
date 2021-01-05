7ab9631d98d97cfee2be724c2e756708a5a98208
Gabriel <61502999+Kzzy1@users.noreply.github.com>
2021-01-05 16:11:27 +0100

    Update connect.dart

--- lib/screens/transition/connect.dart
+++ lib/screens/transition/connect.dart
@@ -75,51 +75,53 @@ class ConnectPage extends State<connectPageState>{
               ),
             ]
         ),
-        body:  Center(
-          child: Container(width: MediaQuery.of(context).size.width*0.8,
-            child: Column(mainAxisAlignment: MainAxisAlignment.center,
-              children: [Row(children: [
-                Expanded(child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text("Code:",
-                    style: TextStyle(fontSize: SizeConfig.TextFontSize, color: setTheme.accentColor,
-                        fontWeight: FontWeight.bold, fontFamily: 'Architect'
+        body:  SingleChildScrollView(
+          child: Center(
+            child: Container(width: MediaQuery.of(context).size.width*0.8,
+              child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                children: [Row(children: [
+                  Expanded(child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Code:",
+                      style: TextStyle(fontSize: SizeConfig.TextFontSize, color: setTheme.accentColor,
+                          fontWeight: FontWeight.bold, fontFamily: 'Architect'
+                      ),
                     ),
                   ),
+                  ),
+                ],
                 ),
-                ),
-              ],
-              ),
-                Row(
-                  children: [
-                    Expanded(child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextField(controller: inputfield, style: TextStyle(color: setTheme.accentColor, fontFamily: 'Architect', fontSize: SizeConfig.TextFieldFontSize), cursorColor: setTheme.accentColor, obscureText: false,
-                        decoration: InputDecoration(enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: setTheme.accentColor)),
-                            border: OutlineInputBorder(), labelStyle: TextStyle(color: setTheme.accentColor),
-                            focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: setTheme.accentColor))
-                        ),
-                      ),
-                    ),),
-                  ],
-                ),
-                  Row(mainAxisAlignment: MainAxisAlignment.center,
+                  Row(
                     children: [
-                      ButtonTheme(minWidth: SizeConfig.SmallButtonWidth, height: SizeConfig.ButtonHeight,
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: RaisedButton(elevation: 3,
-                            onPressed: (){
-                              DatabaseService().buildQuizFromDb(inputfield.text);
-                              Navigator.push(context, new MaterialPageRoute(builder: (context) => new takeQuizPageState(code: x)));
-                            },
-                            color: setTheme.buttonColor,
-                            child: Text("Connect", style: TextStyle(letterSpacing: 1,
-                              fontSize: SizeConfig.ButtonTextSize,fontFamily: 'Architect', color: setTheme.accentColor,)),),
+                      Expanded(child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(controller: inputfield, style: TextStyle(color: setTheme.accentColor, fontFamily: 'Architect', fontSize: SizeConfig.TextFieldFontSize), cursorColor: setTheme.accentColor, obscureText: false,
+                          decoration: InputDecoration(enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: setTheme.accentColor)),
+                              border: OutlineInputBorder(), labelStyle: TextStyle(color: setTheme.accentColor),
+                              focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: setTheme.accentColor))
+                          ),
                         ),
-                      ),
+                      ),),
                     ],
-                  )],),
+                  ),
+                    Row(mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ButtonTheme(minWidth: SizeConfig.SmallButtonWidth, height: SizeConfig.ButtonHeight,
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: RaisedButton(elevation: 3,
+                              onPressed: (){
+                                DatabaseService().buildQuizFromDb(inputfield.text);
+                                Navigator.push(context, new MaterialPageRoute(builder: (context) => new takeQuizPageState(code: x)));
+                              },
+                              color: setTheme.buttonColor,
+                              child: Text("Connect", style: TextStyle(letterSpacing: 1,
+                                fontSize: SizeConfig.ButtonTextSize,fontFamily: 'Architect', color: setTheme.accentColor,)),),
+                          ),
+                        ),
+                      ],
+                    )],),
+            ),
           ),
         )
     );
